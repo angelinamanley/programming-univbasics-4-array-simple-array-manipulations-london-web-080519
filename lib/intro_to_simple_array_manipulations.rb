cf1101fcd49397b6380953c9eb8548d60b78cd48
Angelina Shin Manley <ashin05@gmail.com>
2019-06-26 11:51:50 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,35 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop
+  array.pop
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_concat(array1, array2)
+  array.concat(array2)
+end
+
+def using_insert(array, element)
+  array.insert(element)
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, integer)
+  array.delete(array[integer])
+end
